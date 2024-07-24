8ce918dcb397423fb3ebe60969fde4927b0dd9b1
Daniil Potapov <lancelap@mail.ru>
2024-07-24 16:57:01 +0300

    Fix config

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,8 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { nodePolyfills } from 'vite-plugin-node-polyfills';
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), nodePolyfills()],
-  base: '/my-twa/',
+  base: '/',
 });
