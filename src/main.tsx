456df80f02d195c226ddb6f427dc894e234fa663
Daniil Potapov <lancelap@mail.ru>
2024-07-24 16:56:17 +0300

    Add counter

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,7 @@ import App from './App';
 import './index.css';
 import { TonConnectUIProvider } from '@tonconnect/ui-react';
 
-// this manifest is used temporarily for development purposes
-const manifestUrl = 'https://raw.githubusercontent.com/ton-community/tutorials/main/03-client/test/public/tonconnect-manifest.json';
+const manifestUrl = 'https://lancelap.github.io/my-twa/tonconnect-manifest.json';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <TonConnectUIProvider manifestUrl={manifestUrl}>
